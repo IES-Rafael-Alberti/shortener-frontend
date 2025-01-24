17605cb96736c5c5e026623d69a0be40f62cbad5
JavierAC11 <jacedo@iesrafaelalberti.es>
2025-01-24 09:25:10 +0100

    Pequeños cambios

--- src/utils/fetchMe.jsx
+++ src/utils/fetchMe.jsx
@@ -1,11 +1,14 @@
+import axios from "axios";
+
 const fetchMe = async (token) => {
-        const response = await fetch("http://localhost:3000/me", {
+        const response = await axios.get("http://localhost:3000/me", {
         headers: {
         Authorization: `Bearer ${token}`
         }
     });
-    const data = await response.json();
-    return data;
+    
+    return response.data;
+
     }
 
 export default fetchMe;
