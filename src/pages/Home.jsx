3e9ce1a4e8c142417b174637df61b8e9517ce868
JavierAC11 <jacedo@iesrafaelalberti.es>
2025-01-24 13:51:03 +0100

    Pequeños cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 import axios from "axios";
 import useUserStore from "../stores/useUserStore";
+import { useState } from "react";
 
 const Home = () => {
 
   const user = useUserStore((state) => state.user);
 
+  const [ enlace, setEnlace ] = useState(null);
+
+
+  const handlerVolver = () => {
+    setEnlace(null);
+    document.getElementById("url").value = "";
+  }
+
 
   const handlerGenerarEnlace = async () => {
     if(user.token){
+
+
 
       try {
 
@@ -29,7 +40,24 @@ const Home = () => {
       }
     }
     else {
-      console.log("Debe iniciar sesión");
+      try {
+          
+          const urlEncodedData = new URLSearchParams();
+          urlEncodedData.append("url", document.getElementById("url").value);
+          
+          const response = await axios.post("http://localhost:3000/link", urlEncodedData, {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            }
+          });
+          document.getElementById("url").value = import.meta.env.VITE_DOMAIN+"/"+response.data.code;
+          console.log(user.token);
+          setEnlace(response.data.code);
+      }
+        
+        catch (error) {
+          console.error(error);
+        }
     }
   }
 
@@ -38,7 +66,8 @@ const Home = () => {
     <main className="home">
       <h1 className="home__title">Introduzca su enlace</h1>
       <input type="url" name="url" id="url" className="home__input"/>
-      <button className="home__button" onClick={() => handlerGenerarEnlace()}>Generar enlace</button>
+      {!enlace ? (<button className="home__button" onClick={() => handlerGenerarEnlace()}>Generar enlace</button>)
+      : (<button className="home__button" onClick={() => handlerVolver()}>Volver al generador</button>)}
     </main>
   )
 }
