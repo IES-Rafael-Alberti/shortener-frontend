39568d194d8a7ebe3886bbdf5d782d3b927eab00
Daniel Paz Barroso <dpazbar@g.educaand.es>
2025-01-28 13:14:33 +0100

    Acceso a portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,26 @@
-const Footer = () => {
-    return (
-        <>
-            <footer className="footer">
+import { NavLink } from "react-router";
+import fetchMe from "../utils/fetchMe";
+import useUserStore from "../stores/useUserStore";
+import { useEffect, useState } from "react";
 
-            </footer>
-        </>
+const Footer = () => {
+    const [userData, setUserData] = useState({ email: "" });
+    const user = useUserStore((state) => state.user);
+
+    useEffect(() => {
+        if (user.token) {
+            const fetchUserData = async () => {
+                const data = await fetchMe(user.token); // Llamamos a la función fetchMe con el token del user
+                setUserData(data); // Actualizamos el estado con los datos del user
+            }
+            fetchUserData();
+        }
+    },[])
+
+    return (
+        <footer className="footer">
+            {user.token && <NavLink className="footer__link" to={"/portfolio/"  + userData._id}>Portfolio</NavLink>}
+        </footer>
     );
 };
 
